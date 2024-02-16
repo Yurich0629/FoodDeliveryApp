0efdc4dccc9c179b0852ac02ed33d5073914d99f
Ilya Desiatow <140006151+Yurich0629@users.noreply.github.com>
2024-02-16 19:20:43 +0300

    Created based project and removed storyboard references

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Илья Десятов on 16.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
