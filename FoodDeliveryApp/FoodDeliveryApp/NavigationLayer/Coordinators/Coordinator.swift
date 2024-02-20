154e75251725153f5733e13eb950154622860c57
Ilya Desiatow <140006151+Yurich0629@users.noreply.github.com>
2024-02-20 05:13:49 +0300

    Added onboarding coordinator

--- FoodDeliveryApp/FoodDeliveryApp/NavigationLayer/Coordinators/Coordinator.swift
+++ FoodDeliveryApp/FoodDeliveryApp/NavigationLayer/Coordinators/Coordinator.swift
@@ -22,7 +22,7 @@ protocol CoordinatorProtocol: AnyObject {
     var navigationController: UINavigationController? { get set }
     var finishDelegate: CoordinatorFinishDelegate? { get set }
     
-    func star()
+    func start()
     func finish()
     
 }
@@ -38,4 +38,39 @@ extension CoordinatorProtocol {
 
 protocol CoordinatorFinishDelegate: AnyObject {
     func coordinatorDidFinish(childCoordinator: CoordinatorProtocol)
+}
+
+protocol TabBarCoordinator: AnyObject, CoordinatorProtocol {
+    var tabBarController: UITabBarController? { get set }
+}
+
+class Coordinator: CoordinatorProtocol {
+    
+    var childCoordinators: [CoordinatorProtocol]
+    var type: CoordinatorType
+    var navigationController: UINavigationController?
+    var finishDelegate: CoordinatorFinishDelegate?
+    
+    init(childCoordinators: [CoordinatorProtocol] = [CoordinatorProtocol](), type: CoordinatorType, navigationController: UINavigationController, finishDelegate: CoordinatorFinishDelegate? = nil) {
+        self.childCoordinators = childCoordinators
+        self.type = type
+        self.navigationController = navigationController
+        self.finishDelegate = finishDelegate
+    }
+    
+    deinit {
+        print("Coordinator deinited \(type)")
+        childCoordinators.forEach { $0.finishDelegate = nil }
+        childCoordinators.removeAll()
+    }
+    
+    func start() {
+        print("Coordinator start")
+    }
+    
+    func finish() {
+        print("Coordinator finish")
+    }
+    
+    
 }
